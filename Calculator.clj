6f3826eabfe57eb806bda1ab17055fd1dff41a2a
michal <mp.szmigiel@gmail.com>
2013-05-01 21:18:55 +0300

    Update Calculator.clj

--- Calculator.clj
+++ Calculator.clj
@@ -8,8 +8,17 @@
 (defn mult [a b]
   (println (* a b)))
 
-(defn div [a b]    
-  (println (/ a b)))
+
+(defn div [a b]
+  (if (and (> a 0) (= b 0))
+    "wrong calculating value !!!"  
+    (if (and (= a 0) (> b 0))
+      0   
+  (println (/ a b)))))
 
 (defn modulo [a b]
-  (println (mod a b)))
+  (if (and (> a 0) (= b 0))
+    "wrong calculating value !!!"
+    (if (and (> a 0) (= b 0))
+      0
+  (println (mod a b)))))
