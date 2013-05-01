93794f6981decfa12112887b254b012f86c6e389
michal <mp.szmigiel@gmail.com>
2013-05-01 21:05:51 +0300

    Create mathlogic.clj
    Mathematical logic in clojure

---
+++ mathlogic.clj
@@ -0,0 +1,77 @@
+(ns mathlogic)
+(defn alt [p q]
+  (if (and (= p 1) (= q 1))
+           true
+    (if (and (= p 1) (= q 0))
+           true
+      (if (and (= p 0) (= q 1))
+           true
+        (if (and (= p 0) (= q 0))
+            false 
+             "logical value error")))))
+
+(defn con [p q]
+  (if (and (= p 1) (= q 1))
+           true
+    (if (and (= p 1) (= q 0))
+           false
+      (if (and (= p 0) (= q 1))
+           false
+        (if (and (= p 0) (= q 0))
+            false 
+             "logical value error")))))
+
+(defn imp [p q]
+  (if (and (= p 1) (= q 1))
+           true
+    (if (and (= p 1) (= q 0))
+           false
+      (if (and (= p 0) (= q 1))
+           true
+        (if (and (= p 0) (= q 0))
+            true 
+             "logical value error")))))
+
+(defn iff [p q]
+  (if (and (= p 1) (= q 1))
+           true
+    (if (and (= p 1) (= q 0))
+           false
+      (if (and (= p 0) (= q 1))
+           false
+        (if (and (= p 0) (= q 0))
+            true 
+             "logical value error")))))
+
+(defn nand [p q]
+  (if (and (= p 1) (= q 1))
+           false
+    (if (and (= p 1) (= q 0))
+           true
+      (if (and (= p 0) (= q 1))
+           true
+        (if (and (= p 0) (= q 0))
+            true 
+             "logical value error")))))
+
+(defn xor [p q]
+  (if (and (= p 1) (= q 1))
+           false
+    (if (and (= p 1) (= q 0))
+           true
+      (if (and (= p 0) (= q 1))
+           true
+        (if (and (= p 0) (= q 0))
+            false 
+             "logical value error")))))
+
+(defn nor [p q]
+  (if (and (= p 1) (= q 1))
+           false
+    (if (and (= p 1) (= q 0))
+           false
+      (if (and (= p 0) (= q 1))
+           false
+        (if (and (= p 0) (= q 0))
+            true 
+             "logical value error"))))))
