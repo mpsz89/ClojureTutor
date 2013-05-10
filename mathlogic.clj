16f0e3a772379d12e5be97c28ff3dd4be9c987d0
Michal Piotr Szmigiel <mp.szmigiel@gmail.com>
2013-05-10 14:48:17 +0200

    Update mathlogic.clj

--- mathlogic.clj
+++ mathlogic.clj
@@ -75,3 +75,14 @@
         (if (and (= p 0) (= q 0))
             true 
              "logical value error")))))
+
+(defn xnor [p q]
+  (if (and (= p 1) (= q 1))
+           true
+    (if (and (= p 1) (= q 0))
+           false
+      (if (and (= p 0) (= q 1))
+           false
+        (if (and (= p 0) (= q 0))
+            true 
+             "logical value error")))))
