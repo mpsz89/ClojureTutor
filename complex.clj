c123384ca346c5d943c86892dfb52d739feac702
Michal Piotr Szmigiel <mp.szmigiel@gmail.com>
2014-11-14 16:08:51 +0100

    Update complex.clj

--- complex.clj
+++ complex.clj
@@ -1,7 +1,6 @@
 (ns complex)
 (defn complex
   [re im]
-  ;Postać kanoniczna liczby zespolonej
   (if(and (= re 0) (>= im 0))
           (str im"j")
           (if(and (= re 0) (< im 0))
@@ -11,5 +10,31 @@
                             (if(and (> re 0) (< im 0))
                                     (str "("re""im"j)")
                                     (str "("re"+"im"j)"))))))
+
+(defn planar_complex
+  [a1 a2 b1 b2]
+  (complex (+ a1 b1) (+ a2 b2)))
+
+(defn add_complex
+  [a1 a2 b1 b2]
+  (complex (+ a1 a2) (+ b1 b2)))
+
+(defn sub_complex
+  [a1 a2 b1 b2]
+  (complex (- a1 a2) (- b1 b2)))
+
+(defn mult_complex
+  [a1 a2 b1 b2]
+  (complex (- (* a1 a2) (* b1 b2) ) (+ (* a1 b2) (* a2 b1) )))
+
+(defn div_complex
+  [a1 a2 b1 b2]
+  (complex (/ ((* a1 a2) (* b1 b2)) ((* a2 a2) (* b2 b2))  )  (/ ((* a1 b2) (* a2 b1)) ((* a2 a2) (* b2 b2))  ) ))
+
+(defn modulus
+  [x y]
+  (Math/sqrt (* (re x y) (im x y))))
+                                  
+                                
 
                
