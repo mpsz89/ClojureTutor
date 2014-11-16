fd8fcc612166358b63835b1b19cf36c8e104c298
Michal Piotr Szmigiel <mp.szmigiel@gmail.com>
2014-11-16 13:29:12 +0100

    Update complex.clj

--- complex.clj
+++ complex.clj
@@ -1,4 +1,11 @@
+;Michał Piotr Szmigiel
+;Clojure 1.6.0
+;16 listopada 2014 r.
+;Liczby zespolone w Clojure
+
 (ns complex)
+
+;Tworzenie funkcji liczby zespolonej
 (defn complex
   [re im]
   (if(and (= re 0) (>= im 0))
@@ -11,30 +18,40 @@
                                     (str "("re""im"j)")
                                     (str "("re"+"im"j)"))))))
 
+;Tworzenie funkcji planarnej liczby zespolonej
 (defn planar_complex
   [a1 a2 b1 b2]
   (complex (+ a1 b1) (+ a2 b2)))
 
+;Tworzenie funkcji sumy liczby zespolonej
 (defn add_complex
   [a1 a2 b1 b2]
   (complex (+ a1 a2) (+ b1 b2)))
 
+;Tworzenie funkcji różnicy liczby zespolonej
 (defn sub_complex
   [a1 a2 b1 b2]
   (complex (- a1 a2) (- b1 b2)))
 
+;Tworzenie funkcji iloczynu liczby zespolonej
 (defn mult_complex
   [a1 a2 b1 b2]
   (complex (- (* a1 a2) (* b1 b2) ) (+ (* a1 b2) (* a2 b1) )))
 
+;Tworzenie funkcji ilorazu liczby zespolonej
 (defn div_complex
   [a1 a2 b1 b2]
   (complex (/ (- (* a1 a2) (* b1 b2)) (+ (* a2 a2) (* b2 b2))  )  (/ (+ (* a1 b2) (* a2 b1)) (+ (* a2 a2) (* b2 b2))  ) ))
 
+;Tworzenie funkcji modułu liczby zespolonej
 (defn modulus
-  [x y]
-  (Math/sqrt (* (re x y) (im x y))))
-                                  
-                                
+  [re im]
+  (Math/sqrt (+ (* re re) (* im im))))
 
-               
+;Tworzenie funkcji argumentu liczby zespolonej
+(defn argument [re im]
+  (if (> re 0) (Math/atan (/ im re))
+    (if (and (< re 0) (>= im 0)) (+ Math/PI (Math/atan (/ im re)))
+      (if (and (< re 0) (< im 0)) (- Math/PI (Math/atan (/ im re)))
+        (if (and (= re 0) (> im 0)) (/ Math/PI 2)
+          (if (and (= re 0) (< im 0)) (* (-1) (/ Math/PI 2)) "indeterminate"))))))
