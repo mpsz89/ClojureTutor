3060d57f9b2db60bad0b2d86d2a25f8f60c61bd6
Michal Piotr Szmigiel <mp.szmigiel@gmail.com>
2014-11-16 13:31:01 +0100

    Update ComplexFunction.clj

--- ComplexFunction.clj
+++ ComplexFunction.clj
@@ -1,12 +1,21 @@
+;Michał Piotr Szmigiel
+;Clojure 1.6.0
+;16 listopada 2014 r.
+;funkcji zespolona w Clojure
+
 (ns ComplexFunction)
+
+;Tworzenie funkcji realisa
 (defn re
   [x y]
   (- (* x x) (* y y)))
 
+;Tworzenie funkcji imaginarisa
 (defn im
   [x y]
   (* 2 (* x y)))
 
+;Tworzenie funkcji liczby zespolonej
 (defn complex
   [re im]
   (if(and (= re 0) (>= im 0))
@@ -18,7 +27,7 @@
                             (if(and (> re 0) (< im 0))
                                     (str "("re""im"j)")
                                     (str "("re"+"im"j)"))))))
-
+;Tworzenie funkcji zespolonej
 (defn complex_function
   [x y]
   (complex (re x y) (im x y)))
