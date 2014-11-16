2d748515bdaf1f3d69898ed48079b95934e38122
Michal Piotr Szmigiel <mp.szmigiel@gmail.com>
2014-11-16 21:50:06 +0100

    Update currency.clj

--- currency.clj
+++ currency.clj
@@ -5,42 +5,55 @@
 
 (ns currency)
 
+;Funkcja, która przelicza wartości z polskich złotych na euro
 (defn pln_to_euro [pln]
     (if (>= pln 0) (* pln 0.24) "Value of currency error!"))
-    
+ 
+ ;Funkcja, która przelicza wartości z eura na polskie złote   
 (defn euro_to_pln [euro]
     (if (>= euro 0) (* euro 4.20) "Value of currency error!"))
-   
+
+;Funkcja, która przelicza wartości z polskich złotych na funty brytyjskie   
 (defn pln_to_gbp [pln]
     (if (>= pln 0) (* pln 0.70) "Value of currency error!"))
-    
+
+;Funkcja, która przelicza wartości z funta brytyjskiego na polskie złote     
 (defn gbp_to_pln [gbp]
     (if (>= gbp 0) (* gbp 5.28) "Value of currency error!"))
-   
+
+;Funkcja, która przelicza wartości z funta brytyjskiego na euro   
 (defn gbp_to_euro [gbp]
     (if (>= gbp 0) (* gbp 1.26) "Value of currency error!"))
-    
+
+ ;Funkcja, która przelicza wartości z eura na funty brytyjskie      
 (defn euro_to_gbp [euro]
     (if (>= euro 0) (* euro 2.95) "Value of currency error!"))
 
+;Funkcja, która przelicza wartości z polskich złotych na franki szwajcarskie
 (defn pln_to_chf [pln]
     (if (>= pln 0) (* pln 0.29) "Value of currency error!"))
-    
+
+;Funkcja, która przelicza wartości z franków szwajcarskich na polskie zlote  
 (defn chf_to_pln [chf]
     (if (>= chf 0) (* chf 3.45) "Value of currency error!"))
-   
+
+ ;Funkcja, która przelicza wartości z eura na franki szwajcarskie   
 (defn euro_to_chf [euro]
     (if (>= euro 0) (* euro 1.21) "Value of currency error!"))
-    
+
+;Funkcja, która przelicza wartości z franków szwajcarskich na euro     
 (defn chf_to_euro [chf]
     (if (>= chf 0) (* chf 0.82) "Value of currency error!"))
-   
+
+;Funkcja, która przelicza wartości z funta brytyjskiego na franki szwajcarskie    
 (defn gbp_to_chf [gbp]
     (if (>= gbp 0) (* gbp 1.52) "Value of currency error!"))
-    
+
+;Funkcja, która przelicza wartości z franków szwajcarskich na funty brytyjskie    
 (defn chf_to_gbp [chf]
     (if (>= chf 0) (* chf 0.66) "Value of currency error!"))
 
+;Tworzona funkcja, która przelicza podaną wartość z pierwszej waluty na drugą
 (defn currency [from to value]
   (if (and (= from "pln") (= to "euro")) (str value" pln = "(pln_to_euro value)" euro")
     (if (and (= from "euro") (= to "pln")) (str value" euro = "(euro_to_pln value)" pln")
